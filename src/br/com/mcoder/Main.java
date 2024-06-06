3a7de92faeeefbbf1f76a543152b218131d72cb0
marcelloCoder <deoliveiral767@gmail.com>
2024-06-06 19:06:19 -0300

    Interfaces, options changes

--- src/br/com/mcoder/Main.java
+++ src/br/com/mcoder/Main.java
@@ -20,5 +20,38 @@ public class Main {
                 "DIGITE 5 - SAIR\n",
                 "Cadastro",
                 JOptionPane.INFORMATION_MESSAGE);
+
+
+        while (isOpcaoValida(opcao)){
+
+            if(isOpcaoCadastro(opcao)){
+                String nome = JOptionPane.showInputDialog(null, "Digite os dados do cliente \n-Nome-");
+                String cpf = JOptionPane.showInputDialog(null, "-CPF-");
+                String genero = JOptionPane.showInputDialog(null, "-Genero-");
+                String cidade = JOptionPane.showInputDialog(null, "-Cidade-");
+                String estado = JOptionPane.showInputDialog(null, "-Estado-");
+
+            }else{
+
+            }
+            break;
+        }
+    }
+
+    private static boolean isOpcaoCadastro(String opcao) {
+        if("1".equals(opcao)){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    private static boolean isOpcaoValida(String opcao) {
+
+        if(opcao.isEmpty()){
+            return false;
+        }
+        return true;
     }
 }
