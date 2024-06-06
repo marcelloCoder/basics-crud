09f26b0947141ef193c96f7e42c80170406a26d1
marcelloCoder <deoliveiral767@gmail.com>
2024-06-06 18:29:37 -0300

    Changes interfaces

--- src/br/com/mcoder/Main.java
+++ src/br/com/mcoder/Main.java
@@ -1,12 +1,24 @@
 package br.com.mcoder;
 
+import br.com.mcoder.dao.ClienteMapDAO;
+import br.com.mcoder.dao.IClienteDAO;
 import br.com.mcoder.domain.Cliente;
+
+import javax.swing.*;
 
 public class Main {
 
+    public static IClienteDAO iClienteDAO;
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("Marcello", 10L,"Masculino","Passos","MG");
 
-        System.out.println(cliente);
+        iClienteDAO = new ClienteMapDAO();
+
+        String opcao = JOptionPane.showInputDialog(null, "DIGITE 1 - CADASTRO \n" +
+                "DIGITE 2 - CONSULTAR\n" +
+                "DIGITE 3 - EXCLUIR\n" +
+                "DIGITE 4 - ALTERAR\n" +
+                "DIGITE 5 - SAIR\n",
+                "Cadastro",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 }
--- src/br/com/mcoder/dao/ClienteMapDAO.java
+++ src/br/com/mcoder/dao/ClienteMapDAO.java
@@ -20,26 +20,38 @@ public class ClienteMapDAO implements IClienteDAO{
             return false;
         }
         this.map.put(cliente.getCpf(), cliente);
-        return null;
+        return true;
     }
 
     @Override
     public void excluir(Long cpf) {
-
+        Cliente clienteCadastrado = this.map.get(cpf);
+        if(clienteCadastrado != null){
+            this.map.remove(clienteCadastrado.getCpf(), clienteCadastrado);
+        }
     }
 
     @Override
     public void alterar(Cliente cliente) {
+        Cliente clienteCadastrado = this.map.get(cliente.getCpf());
+        if(clienteCadastrado != null){
+            clienteCadastrado.setNome(cliente.getNome());
+            clienteCadastrado.setCidade(cliente.getCidade());
+            clienteCadastrado.setCpf(cliente.getCpf());
+            clienteCadastrado.setEstado(cliente.getEstado());
+            clienteCadastrado.setGenero(cliente.getGenero());
+
+        }
 
     }
 
     @Override
     public Cliente consultar(Long cpf) {
-        return null;
+        return this.map.get(cpf);
     }
 
     @Override
     public Collection<Cliente> buscarTodos() {
-        return null;
+        return this.map.values();
     }
 }
