db9b9a5ac2493d8339ebb5b5415e64c9b1ebc746
marcelloCoder <deoliveiral767@gmail.com>
2024-06-17 17:31:42 -0300

    Fourth app

--- src/four/br/com/mcoder/entities/Carro.java
+++ src/four/br/com/mcoder/entities/Carro.java
@@ -10,4 +10,54 @@ public class Carro {
     private Double valor;
     private Date data_fabricado;
 
+    public Carro() {
+    }
+
+    public Carro(Long id, String marca, String modelo, Double valor, Date data_fabricado) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.valor = valor;
+        this.data_fabricado = data_fabricado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData_fabricado() {
+        return data_fabricado;
+    }
+
+    public void setData_fabricado(Date data_fabricado) {
+        this.data_fabricado = data_fabricado;
+    }
 }
--- src/four/br/com/mcoder/entities/DocumentoCarro.java
+++ src/four/br/com/mcoder/entities/DocumentoCarro.java
@@ -1,6 +1,34 @@
 package four.br.com.mcoder.entities;
 
-public class DocumentoCarro {
+import java.util.Date;
+
+public class DocumentoCarro extends Carro{
     private String dono;
     private String ipva;
+
+    public DocumentoCarro() {
+        super();
+    }
+
+    public DocumentoCarro(Long id, String marca, String modelo, Double valor, Date data_fabricado, String dono, String ipva) {
+        super(id, marca, modelo, valor, data_fabricado);
+        this.dono = dono;
+        this.ipva = ipva;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public String getIpva() {
+        return ipva;
+    }
+
+    public void setIpva(String ipva) {
+        this.ipva = ipva;
+    }
 }
