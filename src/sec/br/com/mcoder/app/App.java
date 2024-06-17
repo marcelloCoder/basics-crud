f95f6435d4b7eb5df25bc14faf3728a3c1710f2a
marcelloCoder <deoliveiral767@gmail.com>
2024-06-17 15:28:33 -0300

    Third app

--- src/sec/br/com/mcoder/app/App.java
+++ src/sec/br/com/mcoder/app/App.java
@@ -2,11 +2,14 @@ package sec.br.com.mcoder.app;
 
 import sec.br.com.mcoder.entitys.Cliente;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
+
         Locale.setDefault(Locale.US);
         Cliente cliente = new Cliente();
         Scanner sc = new Scanner(System.in);
@@ -14,54 +17,69 @@ public class App {
         String teste1 = sc.nextLine();
 
         while (teste1.equals("1")) {
-            System.out.println("BEM-VINDO");
-            System.out.println("Número da conta");
-            Long numero = sc.nextLong();
-            System.out.println("Nome: ");
-            sc.nextLine();
-            String nome = sc.nextLine();
-            System.out.println("Qual é o seu saldo inicial?");
-            Double saldoInicial = sc.nextDouble();
-            sc.nextLine();
-            System.out.println("Deseja acessar banco?");
-            String resposta = sc.nextLine();
 
 
-            cliente.setCodigo(numero);
-            cliente.setNome(nome);
-            cliente.setSaldo(saldoInicial);
+            try {
+                System.out.println("BEM-VINDO");
+                System.out.println("Número da conta");
+                Long numero = sc.nextLong();
+                System.out.println("Nome: ");
+                sc.nextLine();
+                String nome = sc.nextLine();
+                System.out.println("Qual é o seu saldo inicial?");
+                Double saldoInicial = sc.nextDouble();
+                sc.nextLine();
+                System.out.println("Deseja acessar banco?");
+                String resposta = sc.nextLine();
 
 
-            if (resposta.isEmpty()) {
-                System.out.println("OK, até mais");
-            }
+                cliente.setCodigo(numero);
+                cliente.setNome(nome);
+                cliente.setSaldo(saldoInicial);
 
-            System.out.println("Deseja depositar ou sacar?");
-            String resposta2 = sc.nextLine();
-            if (resposta2.equals("sacar")) {
-                System.out.println("Saldo disponivel: " + cliente.getSaldo());
-                System.out.println("Sacar quanto?");
-                Double sacar = sc.nextDouble();
-                cliente.saque(sacar);
 
-            } else if (resposta2.equals("depositar")) {
-                System.out.println("Saldo disponivel: " + cliente.getSaldo());
-                System.out.println("Depositar quanto?");
-                Double depositar = sc.nextDouble();
-                cliente.deposito(depositar);
-            }else{
-                System.out.println("ATÉ MAIS!");
+                if (resposta.isEmpty()) {
+                    System.out.println("OK, até mais");
+                }else{
+                    System.out.println("Deseja depositar ou sacar?");
+                    String resposta2 = sc.nextLine();
+                    if (resposta2.equals("sacar")) {
+                        System.out.println("Saldo disponivel: " + cliente.getSaldo());
+                        System.out.println("Sacar quanto?");
+                        Double sacar = sc.nextDouble();
+                        cliente.saque(sacar);
+
+                    } else if (resposta2.equals("depositar")) {
+                        System.out.println("Saldo disponivel: " + cliente.getSaldo());
+                        System.out.println("Depositar quanto?");
+                        Double depositar = sc.nextDouble();
+                        cliente.deposito(depositar);
+                    } else {
+                        System.out.println("ATÉ MAIS!");
+
+                    }
+
+                }
+
+
+
+
+            }catch (InputMismatchException e){
+                System.out.println("DIGITE APENAS NUMEROS");
+                e.printStackTrace();
             }
 
 
             System.out.println(cliente);
             System.out.println("DIGITE 1 PARA CONTINUAR OU OUTRO PARA SAIR");
 
+            sc.nextLine();
             teste1 = sc.nextLine();
         }
 
 
         sc.close();
+
     }
 
 }
