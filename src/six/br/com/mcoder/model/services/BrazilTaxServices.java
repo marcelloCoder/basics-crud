a5b93d6341e81305eb3799d45feb52ae736cb75e
marcelloCoder <deoliveiral767@gmail.com>
2024-06-24 14:14:55 -0300

    Six app

--- src/six/br/com/mcoder/model/services/BrazilTaxServices.java
+++ src/six/br/com/mcoder/model/services/BrazilTaxServices.java
@@ -1,6 +1,6 @@
 package six.br.com.mcoder.model.services;
 
-public class BrazilTaxServices {
+public class BrazilTaxServices implements TaxService{
 
     public double tax(double amount){
         if(amount <= 100){
---
+++ src/six/br/com/mcoder/model/services/TaxService.java
@@ -0,0 +1,6 @@
+package six.br.com.mcoder.model.services;
+
+public interface TaxService {
+
+    double tax(double amount);
+}
