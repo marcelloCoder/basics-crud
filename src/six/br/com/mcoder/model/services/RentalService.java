6abcbf17c05bfe3fcfffea2068f3596a0aef3848
marcelloCoder <deoliveiral767@gmail.com>
2024-06-24 13:55:51 -0300

    Six app

--- src/six/br/com/mcoder/model/services/RentalService.java
+++ src/six/br/com/mcoder/model/services/RentalService.java
@@ -3,6 +3,8 @@ package six.br.com.mcoder.model.services;
 import six.br.com.mcoder.model.entities.CarRental;
 import six.br.com.mcoder.model.entities.Invoice;
 
+import java.time.Duration;
+
 public class RentalService {
 
     private Double pricePerHour;
@@ -17,6 +19,16 @@ public class RentalService {
     }
 
     public void processInvoice(CarRental carRental){
-        carRental.setInvoice(new Invoice(50.0,10.0));
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60;
+        double basicPayment;
+        if(hours <= 12.0){
+            basicPayment = pricePerHour * Math.ceil(hours);
+        }else{
+            basicPayment = pricePerDay * Math.ceil(hours / 24.0);
+        }
+
+        double tax = taxService.tax(basicPayment);
+        carRental.setInvoice(new Invoice(basicPayment,tax));
     }
 }
