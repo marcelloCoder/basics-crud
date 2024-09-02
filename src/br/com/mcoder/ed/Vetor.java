b4fc662c309b09df808363937acd31c6764c55fb
marcelloCoder <deoliveiral767@gmail.com>
2024-09-02 20:13:39 -0300

    vetores

---
+++ src/br/com/mcoder/ed/Vetor.java
@@ -0,0 +1,15 @@
+package br.com.mcoder.ed;
+
+public class Vetor {
+    private String[] elementos;
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+        int i = 0;
+        i++;
+    }
+}
