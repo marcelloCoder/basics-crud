43f7bd300d85f3d120978c17f5ee662f315d00aa
marcelloCoder <deoliveiral767@gmail.com>
2024-09-05 21:23:35 -0300

    Lista Encadeada

---
+++ src/br/com/mcoder/ed/ListaEncadeadaTeste.java
@@ -0,0 +1,11 @@
+package br.com.mcoder.ed;
+
+public class ListaEncadeadaTeste {
+
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> listaEncadeada = new ListaEncadeada<>();
+        listaEncadeada.adiciona(1);
+
+        System.out.println(listaEncadeada);
+    }
+}
