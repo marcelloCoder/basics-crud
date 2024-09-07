7eceabd0b183a06db7258b6cea32a4d84ccd1c73
marcelloCoder <deoliveiral767@gmail.com>
2024-09-07 15:39:30 -0300

    already

--- src/br/com/mcoder/model/Train.java
+++ src/br/com/mcoder/model/Train.java
@@ -1,4 +1,20 @@
 package br.com.mcoder.model;
 
 public class Train {
+    static long id = 0;
+
+    static void acessCode(int qntd){
+        for (int i = 0; i < qntd; i++) {
+            id++;
+            System.out.printf("\n id:"+id);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        int quantity = 8;
+        acessCode(quantity);
+    }
+
 }
